a4f6beb71e17c5bb1952cac2f9611268e2a47a8a
Saleem Hadad <saleem_ha@hotmail.com>
2018-01-01 10:20:51 +0800

    Update saveThePrisoner.py

--- saveThePrisoner.py
+++ saveThePrisoner.py
@@ -5,8 +5,7 @@ import sys
 def saveThePrisoner(n, m, s):
     if (m+s-1)%n == 0:
         return n
-    else:
-        return (m+s-1)%n
+    return (m+s-1)%n
 
 t = int(raw_input().strip())
 for a0 in xrange(t):
