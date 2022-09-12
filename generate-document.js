cadaf9887e51f605641963859eae6aaa4d263cc8
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-13 01:54:38 +0400

    generate-document

---
+++ generate-document.js
@@ -0,0 +1,22 @@
+function generateDocument(characters, document) {
+    if(document == "") return true;
+  
+    let charactersCount = {};
+    for(char of characters) {
+      if(char in charactersCount) {
+        charactersCount[char]++;
+      }else {
+        charactersCount[char] = 1;
+      }
+    }
+  
+    for(char of document) {
+      if(char in charactersCount && charactersCount[char] > 0) {
+        charactersCount[char]--;
+      }else {
+        return false;
+      }
+    }
+    
+    return true;
+  }
