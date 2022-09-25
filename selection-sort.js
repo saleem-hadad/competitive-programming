d5e1061efde23be9832f9a1f48a0fbb7a408ee8c
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-25 22:01:07 +0400

    add selection-sort

---
+++ selection-sort.js
@@ -0,0 +1,17 @@
+function selectionSort(array) {
+  for (let index = 0; index < array.length; index++) {
+    let minItemValue = Number.POSITIVE_INFINITY
+    let minItemIndex = index
+    for (let j = index + 1; j < array.length; j++) {
+      if (array[j] < array[minItemIndex]) {
+        minItemIndex = j
+      }
+    }
+
+    let temp = array[index]
+    array[index] = array[minItemIndex]
+    array[minItemIndex] = temp
+  }
+
+  return array
+}
