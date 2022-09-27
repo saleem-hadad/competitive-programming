ec9b6b5a276731ff6b719a9caafc5b2b53c61feb
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-28 03:16:42 +0400

    wip

--- algoexpert/trees.js
+++ algoexpert/trees.js
@@ -5,13 +5,24 @@ class Node {
         this.right = null;
     }
 
+    // depthFirstTraversal() {
+    //     console.log(this.value);
+    //     if(this.left) {
+    //         this.left.depthFirstTraversal();
+    //     }
+    //     if(this.right) {
+    //         this.right.depthFirstTraversal();
+    //     }
+    // }
+
     depthFirstTraversal() {
-        console.log(this.value);
-        if(this.left) {
-            this.left.depthFirstTraversal();
-        }
-        if(this.right) {
-            this.right.depthFirstTraversal();
+        let stack = [ this ]
+
+        while(stack.length > 0) {
+            let current = stack.pop();
+            console.log(current.value);
+            if (current.right) stack.push(current.right)
+            if (current.left) stack.push(current.left)
         }
     }
 
@@ -54,7 +65,88 @@ b.left = d;
 b.right = e;
 
 
-a.swap().depthFirstTraversal();
+// a.depthFirstTraversal();
 
 // console.log(a.contains('f'));
 // console.log(a.contains('z'));
+
+
+// function sum(root) {
+//     if(! root) return 0;
+
+//     return root.value + sum(root.left) + sum(root.right);
+// }
+
+function sum(root) {
+    let total = 0;
+
+    let queue = [ root ];
+
+    while(queue.length > 0) {
+        let current = queue.shift();
+        total += current.value;
+        if(current.left) queue.push(current.left)
+        if(current.right) queue.push(current.right)
+    }
+
+    return total;
+}
+
+// function getSmallest(root) {
+//     let smallest = Infinity;
+
+//     let queue = [ root ];
+
+//     while(queue.length > 0) {
+//         let current = queue.shift();
+        
+//         smallest = Math.min(smallest, current.value);
+        
+//         if(current.left) queue.push(current.left)
+//         if(current.right) queue.push(current.right)
+//     }
+
+//     return smallest;
+// }
+
+
+function getSmallest(root) {
+    if(!root) return Infinity
+    return Math.min(root.value, getSmallest(root.left), getSmallest(root.right));
+}
+
+function getBiggest(root) {
+    if(!root) return -Infinity
+    return Math.max(root.value, getBiggest(root.left), getBiggest(root.right));
+}
+
+function getMaxRootToLeaf(root) {
+    if(!root) return -Infinity;
+    return root.value + Math.max(0, getMaxRootToLeaf(root.left), getMaxRootToLeaf(root.right))
+}
+
+let _3 = new Node(3);
+let _11 = new Node(11);
+let _4 = new Node(4);
+let _2 = new Node(2);
+let _41 = new Node(4);
+let _1 = new Node(1);
+
+_3.left = _11;
+_3.right = _41;
+_41.right = _1;
+_11.left = _4;
+_11.right = _2;
+
+/*
+     3
+    /  \
+   11  4
+   / \ \
+  4  2  1
+*/
+
+// console.log(sum(_3));
+// console.log(getSmallest(_3));
+// console.log(getBiggest(_3));
+console.log(getMaxRootToLeaf(_3))
