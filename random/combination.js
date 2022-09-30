cdaa53403a8cb2e3174cfdba80bd1a4dd6e39a51
Saleem Hadad <saleem.hadad@talabat.com>
2022-10-01 01:35:59 +0400

    wip

--- random/combination.js
+++ random/combination.js
@@ -1,17 +1,17 @@
 // O(2^n)
 // O(n^2)
-// const combinations = (elements) => {
-//     if(elements.length == 0) return [[]];
+const combinations = (elements) => {
+    if(elements.length == 0) return [[]];
 
-//     let last = elements.pop();
-//     let combinationsWithoutLast = combinations(elements)
-//     let combinationsLast = [];
-//     combinationsWithoutLast.forEach(com => {
-//         combinationsLast.push([...com, last])
-//     })
+    let last = elements.pop();
+    let combinationsWithoutLast = combinations(elements)
+    let combinationsLast = [];
+    combinationsWithoutLast.forEach(com => {
+        combinationsLast.push([...com, last])
+    })
     
-//     return [...combinationsWithoutLast, ...combinationsLast]
-// }
+    return [...combinationsWithoutLast, ...combinationsLast]
+}
 
 let result = []
 const combinationsWithK = (elements, k) => {
